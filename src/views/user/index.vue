ba49ad24235afaab06a97879d3c4ef8c099ca53e
jiangzhuo <glmmmmmm@163.com>
2018-10-22 20:31:00 +0800

    余额修改

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -58,13 +58,29 @@
       <el-table-column :label="$t('table.actions')" align="center">
         <template slot-scope="scope">
           <router-link :to="'/user/account/'+scope.row.id">
-            <el-button type="primary" size="small" icon="el-icon-edit">消费明细</el-button>
+            <el-button type="primary" size="small" icon="el-icon-money">消费明细</el-button>
           </router-link>
+          <el-button type="primary" size="small" icon="el-icon-edit" @click="handleChangeBalance(scope.row)">修改余额</el-button>
           <el-button v-if="scope.row.status&0b1" type="info" size="small" icon="el-icon-delete" @click="handleRevertDeleted(scope.row)">恢复账号</el-button>
           <el-button v-else type="danger" size="small" icon="el-icon-delete" @click="handleDelete(scope.row)">封号</el-button>
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog :visible.sync="dialogFormVisible" title="修改余额">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left">
+        <el-form-item :label="$t('table.changeBalanceValue')" prop="changeBalanceValue">
+          <el-input-number v-model="temp.tempChangeBalanceValue"/>
+        </el-form-item>
+        <el-form-item :label="$t('table.changeBalanceExtraInfo')" prop="changeBalanceExtraInfo">
+          <el-input v-model="temp.tempChangeBalanceExtraInfo" type="textarea" autosize placeholder="修改原因"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">{{ $t('table.cancel') }}</el-button>
+        <el-button type="primary" @click="changeBalance">{{ $t('table.confirm') }}</el-button>
+      </div>
+    </el-dialog>
 
     <div class="pagination-container">
       <el-pagination v-show="total>0" :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" :total="total" background layout="total, sizes, prev, pager, next, jumper" @size-change="handleSizeChange" @current-change="handleCurrentChange"/>
@@ -75,6 +91,7 @@
 <script>
 // import { fetchList, createArticle, updateArticle } from '@/api/article'
 import USER_SEARCH from '@/graphqls/userSearch.graphql'
+import USER_CHANGE_BALANCE_BY_ADMIN from '@/graphqls/userChangeBalanceByAdmin.graphql'
 
 export default {
   name: 'UserTable',
@@ -84,6 +101,12 @@ export default {
       list: null,
       total: null,
       listLoading: false,
+      temp: {
+        id: '',
+        tempChangeBalanceValue: 0,
+        tempChangeBalanceExtraInfo: ''
+      },
+      dialogFormVisible: false,
       listQuery: {
         page: 1,
         limit: 20,
@@ -139,13 +162,55 @@ export default {
     },
     handleDelete(row) {
       this.$notify({
-        title: '成功',
-        message: '删除成功',
-        type: 'success',
+        title: '点击成功',
+        message: '功能还没做',
+        type: 'warn',
         duration: 2000
       })
-      const index = this.list.indexOf(row)
-      this.list.splice(index, 1)
+    },
+    resetTemp() {
+      this.temp = {
+        id: '',
+        tempChangeBalanceValue: 0,
+        tempChangeBalanceExtraInfo: ''
+      }
+    },
+    handleChangeBalance(row) {
+      this.resetTemp()
+      this.temp.id = row.id
+      this.dialogFormVisible = true
+    },
+    async changeBalance() {
+      const result = await this.$apollo.mutate({
+        // 查询语句
+        mutation: USER_CHANGE_BALANCE_BY_ADMIN,
+        // 参数
+        variables: {
+          userId: this.temp.id,
+          changeValue: this.temp.tempChangeBalanceValue,
+          extraInfo: this.temp.tempChangeBalanceExtraInfo
+        }
+      })
+      if (result.data.changeBalanceByAdmin.code !== 200) {
+        await this.getList()
+        this.listLoading = false
+        this.$notify({
+          title: '失败',
+          message: result.data.changeBalanceByAdmin.message,
+          type: 'fail',
+          duration: 2000
+        })
+      } else {
+        await this.getList()
+        this.listLoading = false
+        this.dialogFormVisible = false
+        this.$notify({
+          title: '成功',
+          message: '更新成功',
+          type: 'success',
+          duration: 2000
+        })
+      }
     }
   }
 }
