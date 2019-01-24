8223934de3772b236a0422953457b241b1f34dbf
jiangzhuo <glmmmmmm@163.com>
2019-01-24 16:10:59 +0800

    admin首页展示用户数

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,69 +1,173 @@
 <template>
   <div class="dashboard-editor-container">
-    <div class=" clearfix">
-      <pan-thumb :image="avatar" style="float: left"> Your roles:
-        <span v-for="item in roles" :key="item" class="pan-info-roles">{{ item }}</span>
-      </pan-thumb>
-      <github-corner style="position: absolute; top: 0px; border: 0; right: 0;"/>
-      <div class="info-container">
-        <span class="display_name">{{ name }}</span>
-        <span style="font-size:20px;padding-top:20px;display:inline-block;">Editor's Dashboard</span>
-      </div>
-    </div>
-    <div>
-      <img :src="emptyGif" class="emptyGif">
-    </div>
+
+    <el-row :gutter="40" class="panel-group">
+      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+        <div class="card-panel">
+          <div class="card-panel-icon-wrapper icon-people">
+            <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          </div>
+          <div class="card-panel-description">
+            <div class="card-panel-text">User Count</div>
+            <count-to :start-val="0" :end-val="userCount" :duration="500" class="card-panel-num"/>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <!--<el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">-->
+    <!--<line-chart :chart-data="lineChartData"/>-->
+    <!--</el-row>-->
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import PanelGroup from './components/PanelGroup'
+import LineChart from './components/LineChart'
+import CountTo from 'vue-count-to'
+import STATS_USER_COUNT from '@/graphqls/statsUserCount.graphql'
+
+// const lineChartData = {
+//   newVisitis: {
+//     expectedData: [100, 120, 161, 134, 105, 160, 165],
+//     actualData: [120, 82, 91, 154, 162, 140, 145]
+//   },
+//   messages: {
+//     expectedData: [200, 192, 120, 144, 160, 130, 140],
+//     actualData: [180, 160, 151, 106, 145, 150, 130]
+//   },
+//   purchases: {
+//     expectedData: [80, 100, 121, 104, 105, 90, 100],
+//     actualData: [120, 90, 100, 138, 142, 130, 130]
+//   },
+//   shoppings: {
+//     expectedData: [130, 140, 141, 142, 145, 150, 160],
+//     actualData: [120, 82, 91, 154, 162, 140, 130]
+//   }
+// }
 
 export default {
   name: 'DashboardAdmin',
   components: {
+    CountTo,
+    PanelGroup,
+    LineChart
   },
   data() {
-    return {}
+    return {
+      // lineChartData: lineChartData.newVisitis
+      userCount: 0
+    }
   },
-  computed: {
-    ...mapGetters([
-      'name',
-      'avatar',
-      'roles'
-    ])
+  async created() {
+    await this.getUserCount()
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      // this.lineChartData = lineChartData[type]
+    },
+    async getUserCount() {
+      const result = await this.$apollo.query({
+        // 查询语句
+        query: STATS_USER_COUNT,
+        // 参数
+        variables: {
+          from: 1,
+          to: parseInt(Date.now() / 1000)
+        },
+        fetchPolicy: 'network-only' // 只从网络获取
+      })
+      // const getUserPromises = result.data.getDiscountByPageAndLimit.data.map((discount) => {
+      //   return this.getUser(discount.author)
+      // })
+      // await Promise.all(getUserPromises)
+      this.userCount = result.data.userCount.data
+    }
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .emptyGif {
-    display: block;
-    width: 45%;
-    margin: 0 auto;
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
   }
+}
+</style>
 
-  .dashboard-editor-container {
-    background-color: #e3e3e3;
-    min-height: 100vh;
-    padding: 50px 60px 0px;
-    .pan-info-roles {
-      font-size: 12px;
-      font-weight: 700;
-      color: #333;
-      display: block;
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .panel-group {
+    margin-top: 18px;
+    .card-panel-col{
+      margin-bottom: 32px;
     }
-    .info-container {
+    .card-panel {
+      height: 108px;
+      cursor: pointer;
+      font-size: 12px;
       position: relative;
-      margin-left: 190px;
-      height: 150px;
-      line-height: 200px;
-      .display_name {
+      overflow: hidden;
+      color: #666;
+      background: #fff;
+      box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
+      border-color: rgba(0, 0, 0, .05);
+      &:hover {
+        .card-panel-icon-wrapper {
+          color: #fff;
+        }
+        .icon-people {
+          background: #40c9c6;
+        }
+        .icon-message {
+          background: #36a3f7;
+        }
+        .icon-money {
+          background: #f4516c;
+        }
+        .icon-shopping {
+          background: #34bfa3
+        }
+      }
+      .icon-people {
+        color: #40c9c6;
+      }
+      .icon-message {
+        color: #36a3f7;
+      }
+      .icon-money {
+        color: #f4516c;
+      }
+      .icon-shopping {
+        color: #34bfa3
+      }
+      .card-panel-icon-wrapper {
+        float: left;
+        margin: 14px 0 0 14px;
+        padding: 16px;
+        transition: all 0.38s ease-out;
+        border-radius: 6px;
+      }
+      .card-panel-icon {
+        float: left;
         font-size: 48px;
-        line-height: 48px;
-        color: #212121;
-        position: absolute;
-        top: 25px;
+      }
+      .card-panel-description {
+        float: right;
+        font-weight: bold;
+        margin: 26px;
+        margin-left: 0px;
+        .card-panel-text {
+          line-height: 18px;
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 16px;
+          margin-bottom: 12px;
+        }
+        .card-panel-num {
+          font-size: 20px;
+        }
       }
     }
   }
